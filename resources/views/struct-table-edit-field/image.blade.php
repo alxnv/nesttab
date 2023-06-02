2d07108f51b1e5efa94b5b502f17735fcb0c196e
Alexander Vorobyov <alxnbv@gmail.com>
2023-06-02 14:54:44 +0300

    image strurcture transformation table (beginned)

--- resources/views/struct-table-edit-field/image.blade.php
+++ resources/views/struct-table-edit-field/image.blade.php
@@ -14,6 +14,7 @@ if (isset($r['allowed'])) {
     $r['allowed'] = '';
 }
 
+$image_params = [[0, 0, 'contain'], [200, 150, 'contain']];
 
 echo '<a href="' . $yy->baseurl . 'nesttab/struct-change-table/edit/' . $tbl['id'] . '/0">'
         .__('Back') . '</a><br /><br />';
@@ -43,6 +44,8 @@ if (isset($r['is_error'])) {
     //\app\core\Helper::assignData($r, $_SESSION[$lnk_data]); // читаем сохраненные данные формы
 }
 
+?>
+<?php
 if (isset($r['opt_fields'])) {
     $optOpened = true;
 } else {
@@ -71,10 +74,10 @@ echo $e->getErr('allowed');
 echo __('Allowed extensions') . ': <input type="text" size="50" id="allowed"'
         . ' name="allowed" value="' . (isset($r['allowed']) ? \yy::qs($r['allowed']) : '') . '" />'
         . '<br />';
-echo '<div class="comment">(' . __("Enter file extensions delimeted by ','") . 
-        ', ' . __("or empty string, if any extension is allowed") . '<br />' . 
+echo '<div class="comment"><span class="red">*</span> ' . __("Enter file extensions delimeted by ','") . 
+        ', ' . __("or empty string, if any extension is allowed") . '.<br />' . 
         __("Possible extensions") . ': gif, jpeg, jpg, png' .
-         ')<br /></div>';
+         '.<br /></div>';
 ?>
 <div id="app">
 <input type="checkbox" name="opt_fields" id="opt_fields" v-model="checked" /> <label for="opt_fields"><?=__('Additional fields')?></label><br />
@@ -88,7 +91,38 @@ echo  '<input id="required" type="checkbox"'
         . ' name="req" ' .(isset($r['req']) ? 'checked="checked"' : '') . ' />'
         . ' <label for="required">' . __ ('Is required') .'</label><br />';
 ?>
+<hr />
+<p class="center"><?=__("Image transformations")?></p>
+<br />
+    <template v-for="(field, index) in fields" />
+    <div v-if="index > 1"><button>-</button></div>
+    <table class="table">
+        <th colspan="2">
+            @{{captions[index]}}
+        </th>
+        <tr>
+            <td><?=__('Width, pixels')?>:</td>
+            <td><input type="number" name="i_width[]" class="img_width" v-model="field.width" /></td>
+        </tr>
+        <tr>
+            <td><?=__('Height, pixels')?>:</td>
+            <td><input type="number" name="i_height[]" class="img_height" v-model="field.height" /></td>
+        </tr>
+        <tr>
+            <td><?=__("Transformation type")?>:</td>
+            <td><select v-model="field.type" name="i_type[]">
+                    <option value="cover">Cover</option>
+                    <option value="contain">Contain</option>
+            </select>
+            </td>
+        </tr>
+    </table>
+    <div v-if="index == 0" class="comment"><span class="red">*</span> <?=__("If width = 0 and height = 0 for the main image, the image is loaded as is, without size transformation")?></div>
+    <div v-if="index > 0"><input type="button" value="+" @click="add(index)" /></div>
+    <br />    
+    </template>
 </div>
+
 </div>
 <br />
 <p align="left">
@@ -102,11 +136,35 @@ echo '</form>';
 const app = Vue.createApp({
   data() {
     return {
-      checked: <?=($optOpened ? 'true' : 'false')?>
+      checked: <?=($optOpened ? 'true' : 'false')?>,
+    fields: [
+<?php
+for ($i = 0; $i < count($image_params); $i++) {
+    echo "{
+        width: " . $image_params[$i][0] . ",
+        height: " . $image_params[$i][1] . ",
+        type: '" . $image_params[$i][2] . "'},
+            "; 
+}
+?>
+    ],
+    captions: [
+      "<?=__("Main image")?>",
+      "Thumbnail",
+      "<?=__("Image")?> 1",
+      "<?=__("Image")?> 2",
+      "<?=__("Image")?> 3",
+    ],
     }
+  },
+  methods: {
+    add(index) {
+      alert(index);
+  },  
   }
 });
 app.mount('#app');
+
 </script>
 
 @endsection
