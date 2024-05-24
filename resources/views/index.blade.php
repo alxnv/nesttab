c2e40c18109de78007b66630f09d441481dceb67
Alexander Vorobyov <alxnbv@gmail.com>
2024-05-24 15:06:55 +0300

    todo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,8 @@ $pppp = 1;
 @extends(config('nesttab.layout'))
 @section('content')
 <?php
+/*$cf = config('nesttab.jlkjkjl');
+var_dump($cf);*/
 if (!extension_loaded('gd')) {
     echo '<div class="error">' . sprintf(__("PHP extension '%s' has not been loaded"), 'gd') 
           . '<br />'
